a57a4f7895c492892e695f8d5bffe5f156040d7f
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-26 16:38:56 +0100

    update

--- src/Components/ProductPreviewCard/ProductPreviewCard.jsx
+++ src/Components/ProductPreviewCard/ProductPreviewCard.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ProductPreviewCard = () => {
-  console.log(window);
+  const [BP] = useBP(650);
+
   return (
     <>
       <div
         style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: BP ? "row" : "column",
           backgroundColor: "red",
-          width: "300px",
-          minwidth: "300px",
-          aspectRatio: "10/16",
+          minWidth: BP ? 600 : 300,
+          aspectRatio: BP ? 16 / 10 : 10 / 16,
           borderRadius: "10px",
         }}
-      ></div>
+      >
+        <div
+          style={{
+            backgroundImage: "url(https://picsum.photos/200)",
+            backgroundSize: "cover",
+            height: BP ? "100%" : "50%",
+            width: BP ? "50%" : "100%",
+            borderRadius: BP ? "10px 0px 0px 10px" : "10px 10px 0px 0px",
+          }}
+        >
+          1
+        </div>
+        <h1>2</h1>
+      </div>
     </>
   );
 };
 
 export default ProductPreviewCard;
+
+const useBP = (width = 0) => {
+  const [matches, setMatches] = useState(
+    window.matchMedia(`(min-width: ${width}px)`).matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia(`(min-width: ${width}px)`)
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+  return [matches];
+};
--- src/Components/WindowData/WindowData.jsx
+++ src/Components/WindowData/WindowData.jsx
@@ -7,11 +7,31 @@ const WindowData = () => {
   const [IW, setIW] = useState(0);
   const [IH, setIH] = useState(0);
   const [SY, setSY] = useState(0);
+  const [CX, setCX] = useState(0);
+  const [CY, setCY] = useState(0);
+  const [MX, setMX] = useState(0);
+  const [MY, setMY] = useState(0);
+  const [BT, setBT] = useState(0);
+  const [CTRLKey, setCTRLKey] = useState(0);
+  const [ALTKey, setALTKey] = useState(0);
+  const [SHIFTKey, setSHIFTKey] = useState(0);
 
   useEffect(() => {
-    const timer = setTimeout(() => handleRefresh(), 10);
+    const timer = setTimeout(() => handleRefresh(), 0);
     return () => clearTimeout(timer);
   }, [counter]);
+
+  const handleMovement = (e) => {
+    console.log(e);
+    setCX(e.clientX);
+    setCY(e.clientY);
+    setMX(e.movementX);
+    setMY(e.movementY);
+    setBT(e.buttons);
+    setCTRLKey(e.ctrlKey);
+    setALTKey(e.altKey);
+    setSHIFTKey(e.shiftKey);
+  };
 
   const handleRefresh = () => {
     //console.log(window);
@@ -24,7 +44,19 @@ const WindowData = () => {
   };
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
+      <div
+        onMouseMove={handleMovement}
+        style={{
+          position: "fixed",
+          width: `${IW - 19}px`,
+          height: `${IH - 2}px`,
+          top: 0,
+          left: 0,
+          border: "0.1px solid red",
+          overflow: "hidden",
+        }}
+      ></div>
       <div
         style={{
           position: "fixed",
@@ -52,51 +84,50 @@ const WindowData = () => {
           Scroll + 100
         </button>
       </div>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
-      <h1>...</h1>
+      <div
+        style={{
+          position: "fixed",
+          right: "0px",
+          top: "0px",
+          margin: "10px",
+          backgroundColor: "#555",
+          padding: "5px",
+          borderRadius: "5px",
+        }}
+      >
+        <h4 style={{ color: "antiquewhite" }}>Event.ClientX: {CX}</h4>
+        <h4 style={{ color: "antiquewhite" }}>Event.ClientY: {CY}</h4>
+        <h4 style={{ color: "antiquewhite" }}>Event.MovementX: {MX}</h4>
+        <h4 style={{ color: "antiquewhite" }}>Event.MovementY: {MY}</h4>
+        <h4 style={{ color: "antiquewhite" }}>Event.Button: {BT}</h4>
+        <h4 style={{ color: "antiquewhite" }}>
+          CTRL Key: {CTRLKey ? "🌝" : "🌚"}
+        </h4>
+        <h4 style={{ color: "antiquewhite" }}>
+          ALT Key : {ALTKey ? "🌝" : "🌚"}
+        </h4>
+        <h4 style={{ color: "antiquewhite" }}>
+          SHIFT Key: {SHIFTKey ? "🌝" : "🌚"}
+        </h4>
+      </div>
+      <div
+        style={{
+          backgroundImage: "url(https://picsum.photos/200)",
+          backgroundPosition: `${-1 * SL}px  ${-1 * ST + SY}px`,
+          width: "300px",
+          height: "300px",
+          border: "0.1px solid red",
+        }}
+      ></div>
+      <div
+        style={{
+          backgroundImage: "url(https://picsum.photos/200)",
+          backgroundPosition: `${CX * 0.2}px  ${CY * 0.2}px`,
+          width: "300px",
+          height: "300px",
+          border: "0.1px solid red",
+        }}
+      ></div>
     </div>
   );
 };
