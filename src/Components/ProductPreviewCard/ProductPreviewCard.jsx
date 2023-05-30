65c01a56b468cd8a36f088d6ae231253e9c62f98
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-05-30 13:29:44 +0100

    update

--- src/Components/ProductPreviewCard/ProductPreviewCard.jsx
+++ src/Components/ProductPreviewCard/ProductPreviewCard.jsx
@@ -1,50 +1,44 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
 const ProductPreviewCard = () => {
-  const [BP] = useBP(650);
-
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexDirection: BP ? "row" : "column",
-          backgroundColor: "red",
-          minWidth: BP ? 600 : 300,
-          aspectRatio: BP ? 16 / 10 : 10 / 16,
-          borderRadius: "10px",
-        }}
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1.1, 1] }}
+        exit={{ scale: 0 }}
+        transition={{ duration: 0.5 }}
+        className="w-64 lg:w-[500px] lg:h-96 rounded-lg bg-white flex flex-col lg:flex-row"
       >
-        <div
-          style={{
-            backgroundImage: "url(https://picsum.photos/200)",
-            backgroundSize: "cover",
-            height: BP ? "100%" : "50%",
-            width: BP ? "50%" : "100%",
-            borderRadius: BP ? "10px 0px 0px 10px" : "10px 10px 0px 0px",
-          }}
-        >
-          1
+        <img
+          className="rounded-t-lg lg:rounded-tr-none lg:rounded-l-lg aspect-video lg:aspect-auto lg:w-1/2 object-cover"
+          src="https://picsum.photos/200"
+          alt=""
+        />
+        <div className="m-4 flex flex-col lg:w-1/2 lg:justify-around ">
+          <p className="text-gray-400 font-mono text-xs">Perfume</p>
+          <p className="my-2 font-serif font-bold text-2xl leading-5">
+            Gabriell Esence <br /> Eau De Parfum
+          </p>
+          <p className="text-xs">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+            excepturi, vel
+          </p>
+          <hr className="my-4 border-transparent " />
+          <div className="flex align-top my-2">
+            <span className="font-bold text-2xl text-green-700">$149.99</span>
+            <span className="mx-4 line-through  text-gray-400  text-xs">
+              $169.99
+            </span>
+          </div>
+          <button className="py-2 block w-full rounded text-white text-sm bg-green-700">
+            Add to Cart
+          </button>
         </div>
-        <h1>2</h1>
-      </div>
+      </motion.div>
     </>
   );
 };
 
 export default ProductPreviewCard;
-
-const useBP = (width = 0) => {
-  const [matches, setMatches] = useState(
-    window.matchMedia(`(min-width: ${width}px)`).matches
-  );
-
-  useEffect(() => {
-    window
-      .matchMedia(`(min-width: ${width}px)`)
-      .addEventListener("change", (e) => setMatches(e.matches));
-  }, []);
-  return [matches];
-};
